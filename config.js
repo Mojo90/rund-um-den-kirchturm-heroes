3dbde50323347be9fb76ce662da11b5a31c5da29
Moritz Pfeiffer <mo@develapp.me>
2017-05-24 12:29:49 +0200

    crawler years will now be process variables

--- config.js
+++ config.js
@@ -1,8 +1,13 @@
 var config = {};
 
+config.defaultEnvironment = "production";
+
 config.environment = process.env.environment || config.defaultEnvironment;
 
 config.oneYear = 365 * 24 * 60 * 60 * 1000;
 config.oneDay = 24 * 60 * 60 * 1000;
+
+config.crawlFromYear = process.env.CRAWLER_FROM_YEAR || "2005";
+config.crawlToYear = process.env.CRAWLER_TO_YEAR || "2017";
 
 module.exports = config;
